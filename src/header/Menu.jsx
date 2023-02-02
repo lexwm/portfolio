518de85055a53e8da0aae24a6636ebbc4186ac49
Aleksey Ranuk <ar@snov.io>
2023-02-02 14:27:23 +0200

    menu active link

--- src/header/Menu.jsx
+++ src/header/Menu.jsx
@@ -1,16 +1,17 @@
 import Nav from 'react-bootstrap/Nav';
 import MenuItem from "./MenuItem";
+import MenuData from "./MenuData";
+
 
 function Menu() {
     return (
         <nav id="navbar" className="nav-menu navbar">
             <Nav defaultActiveKey="#hero" as="ul" bsPrefix="left-menu">
-                <MenuItem name="Home" href="#hero" class="bx bx-home"/>
-                <MenuItem name="About" href="#about" class="bx bx-user"/>
-                <MenuItem name="Resume" href="#resume" class="bx bx-file-blank"/>
-                <MenuItem name="Portfolio" href="#portfolio" class="bx bx-book-content"/>
-                <MenuItem name="Services" href="#services" class="bx bx-server"/>
-                <MenuItem name="Contact" href="#contact" class="bx bx-envelope"/>
+                {
+                    MenuData.map(Item => (
+                        <MenuItem name={Item.name} href={Item.href} class={Item.iconClass}/>
+                    ))
+                }
             </Nav>
         </nav>
     );
---
+++ src/header/MenuData.jsx
@@ -0,0 +1,34 @@
+const MenuData = [
+    {
+        name: "Home",
+        href: "#hero",
+        iconClass: "bx bx-home"
+    },
+    {
+        name: "About",
+        href: "#about",
+        iconClass: "bx bx-user"
+    },
+    {
+        name: "Resume",
+        href: "#resume",
+        iconClass: "bx bx-file-blank"
+    },
+    {
+        name: "Portfolio",
+        href: "#portfolio",
+        iconClass: "bx bx-book-content"
+    },
+    {
+        name: "Services",
+        href: "#services",
+        iconClass: "bx bx-server"
+    },
+    {
+        name: "Contact",
+        href: "#contact",
+        iconClass: "bx bx-envelope"
+    }
+];
+
+export default MenuData;
