b0cff6767c7b24242523ca5cd73121c52d8fa81d
Aleksey Ranuk <ar@snov.io>
2023-02-04 10:12:03 +0200

    add footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -6,7 +6,7 @@ function Footer () {
                 {/*    &copy; Copyright <strong><span>iPortfolio</span></strong>*/}
                 {/*</div>*/}
                 <div className="credits">
-                    Powered by <a href="https://reactjs.org/"><i className="bx bxl-react bx-flip-vertical"></i> React</a>
+                    Website powered by <a href="https://reactjs.org/"><i className="bx bxl-react bx-flip-vertical"></i> React</a>
                 </div>
             </div>
         </footer>
